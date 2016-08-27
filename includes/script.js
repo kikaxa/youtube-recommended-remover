d14d8391dae8bac5c22d2211233b651f230790e7
kikaxa <antony.kikaxa@gmail.com>
2016-08-27 16:16:30 +0300

    removal for new interface too

--- includes/script.js
+++ includes/script.js
@@ -8,31 +8,49 @@
 // @include https://*.youtube.com/*
 // ==/UserScript==
 
+hideElement0 = function(e) {
+    if (e === null)
+        return;
+    e.style.display = "none";
+    e.id = "asd2";
+    e.setAttribute("class", "asd");
+    e.parentNode.removeChild(e);
+    //console.log(e, e.parentNode);
+}
+
 removeBleagh = function() {
     //class="video-list-item"
      //class="content-wrapper"
       //class="stat view-count"
 
     var list = document.getElementById('watch-related');
+    var new_iface = false;
+    if (list === null) {
+        new_iface = true;
+        list = document.getElementById('items');
+        hideElement0(document.getElementById('player-ads'));
+    }
+
+    if (list === null) {
+        console.log("again changed interface!");
+        return;
+    }
     var c, j = 0;
     do {
-        var list2 = list.getElementsByClassName("view-count");
+        var list2 = list.getElementsByClassName(new_iface ? "yt-formatted-string-0" : "view-count");
         var i;
         c = 0;
         j++;
         for (i = 0; i < list2.length; i++) {
-            if (list2[i].textContent == "Recommended for you") {
+            if ((!new_iface || list2[i].id == "views") &&
+                    list2[i].textContent == "Recommended for you") {
                 c++;
 
                 var e = list2[i];
                 while (e.parentNode && e.parentNode != list)
                     e = e.parentNode;
 
-                //list2[i].style.display = "none";
-                e.style.display = "none";
-                e.setAttribute("class", "asd");
-                list.removeChild(e);
-                //console.log(e, e.parentNode);
+                hideElement0(e);
             }
         }
         console.log(c, list2.length);
