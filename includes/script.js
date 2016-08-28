c5c021e37631fddb1e89fddea8a25464b98c3c74
kikaxa <antony.kikaxa@gmail.com>
2016-08-28 21:42:02 +0300

    support dynamic updates of page & list

--- includes/script.js
+++ includes/script.js
@@ -18,12 +18,15 @@ hideElement0 = function(e) {
     //console.log(e, e.parentNode);
 }
 
+var observer = null;
+
 removeBleagh = function() {
     //class="video-list-item"
      //class="content-wrapper"
       //class="stat view-count"
 
     var list = document.getElementById('watch-related');
+    var list_more = document.getElementById('watch-more-related');
     var new_iface = false;
     if (list === null) {
         new_iface = true;
@@ -32,9 +35,13 @@ removeBleagh = function() {
     }
 
     if (list === null) {
-        console.log("again changed interface!");
+        console.log("related list not found. new interface? or just search page...");
         return;
     }
+    if (observer === null)
+        observer = new MutationObserver(removeBleagh);
+    observer.disconnect();
+
     var c, j = 0;
     do {
         var list2 = list.getElementsByClassName(new_iface ? "yt-formatted-string-0" : "view-count");
@@ -47,21 +54,34 @@ removeBleagh = function() {
                 c++;
 
                 var e = list2[i];
-                while (e.parentNode && e.parentNode != list)
+                while (e.parentNode && e.parentNode != list && e.parentNode != list_more)
                     e = e.parentNode;
 
                 hideElement0(e);
             }
         }
-        console.log(c, list2.length);
+        if (c)
+            console.log("removed ads", c, "of", list2.length);
     } while (c != 0 && j < 10); //WHAT THE SPINNIN' FUCK?! how many times do you need?
+
+    var config = { childList: true, subtree: true };
+    observer.observe(list, config);
+}
+
+var oldURL = "";
+function checkURLchange(currentURL){
+    if(currentURL != oldURL){
+        oldURL = currentURL;
+        setTimeout(removeBleagh, 100);
+    }
 }
 
 document.addEventListener( "DOMContentLoaded",
 function () {
 
     removeBleagh();
-    //one time is not enough :/
-    setTimeout(removeBleagh, 1000);
 
+    oldURL = window.location.href;
+    setInterval(function() {checkURLchange(window.location.href);
+           }, 1000);
 }, false )
